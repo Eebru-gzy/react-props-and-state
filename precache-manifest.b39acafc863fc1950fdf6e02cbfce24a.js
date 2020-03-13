298f584b1ec577d3545ed51ec5ca70d1206acb8c
Ibrahim <Alaoopeyemi101@gmail.com>
2020-03-13 13:09:35 +0100

    Updates

---
+++ precache-manifest.b39acafc863fc1950fdf6e02cbfce24a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfe89b9dee8182f5fe697a85bafd120",
+    "url": "/react-props-and-state/index.html"
+  },
+  {
+    "revision": "79e8b8a026141d61dd72",
+    "url": "/react-props-and-state/static/css/main.5a456527.chunk.css"
+  },
+  {
+    "revision": "be9cf0ce5081207bed39",
+    "url": "/react-props-and-state/static/js/2.90b7bb36.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-props-and-state/static/js/2.90b7bb36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e8b8a026141d61dd72",
+    "url": "/react-props-and-state/static/js/main.a23433c4.chunk.js"
+  },
+  {
+    "revision": "23cb16d95256dff287f2",
+    "url": "/react-props-and-state/static/js/runtime-main.574195b9.js"
+  }
+]);
